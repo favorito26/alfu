a33ac5bb747c223dac87d931a6e0c6dbc34afd0c
favorito26 <mufaddal2003@gmail.com>
2024-09-01 13:08:24 +0530

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "created for fatema kanore",
-  description: "you are cutest ",
+  title: "created for alefiyah khopoliwala",
+  description: "you are the cutest",
 };
 
 export default function RootLayout({ children }) {
